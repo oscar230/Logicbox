78b60e368a16be54a185ec6c471760f63b54cbd0
Oscar Andersson <2kafkimc@duck.com>
2022-04-27 20:00:06 +0200

    Adds rekordbox helper

---
+++ Logicbox.App/Helpers/RekordboxHelper.cs
@@ -0,0 +1,10 @@
+﻿using Logicbox.App.Models.Rekordbox;
+
+namespace Logicbox.App.Helpers
+{
+    internal static class RekordboxHelper
+    {
+        internal static Library GetLibrary(FileInfo libraryFileInfo) => XmlHelper.Deserialize<Library>(libraryFileInfo);
+        internal static Library GetLibrary(string libraryFileName) => XmlHelper.Deserialize<Library>(libraryFileName);
+    }
+}
