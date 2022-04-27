7366b3a3b57ac762e746ea82c5dc54e4bedef0aa
Oscar Andersson <2kafkimc@duck.com>
2022-04-27 19:59:06 +0200

    Basic interface

---
+++ Logicbox.App/Program.cs
@@ -0,0 +1,21 @@
+﻿using Logicbox.App.Helpers;
+using Logicbox.App.Models.Rekordbox;
+using System.Reflection;
+
+
+ConsoleHelper.WriteLine("Logicbox");
+string? runningDirectoryAsString = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+if (runningDirectoryAsString is null)
+{
+    throw new Exception("Could not the running process's running directory.");
+}
+DirectoryInfo? runningDirectory = new FileInfo(runningDirectoryAsString).Directory;
+if (runningDirectory is null || !runningDirectory.Exists)
+{
+    throw new DirectoryNotFoundException();
+}
+ConsoleHelper.WriteLine(runningDirectory.FullName);
+string[] xmlFileNames = Directory.GetFiles(runningDirectory.FullName, "*.xml", SearchOption.AllDirectories);
+string xmlFileName = xmlFileNames[ConsoleHelper.Selection(xmlFileNames, "Choose you library xml file: ")];
+Library library = RekordboxHelper.GetLibrary(xmlFileName);
+ConsoleHelper.WriteLine($"The library is from {library.Product?.Name} version {library.Product?.Version} and contains {library.Collection?.Entries} tracks.");
